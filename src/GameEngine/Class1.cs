25e19d3b1c26ac2c17a32c752c05659828e6407d
ERIK\Erik <eriksandstrom@outlook.com>
2019-01-08 16:02:55 +0100

    Gameengine

---
+++ src/GameEngine/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameEngine
+{
+    public class Player
+    {
+        public string Name;
+        public Piece Piece;
+
+    }
+
+    public class Piece
+    {
+        public string Name;
+        public int locationX;
+        public int locationY;
+
+    }
+
+    public class Dice
+    {
+
+    }
+
+    public class Rules
+    {
+
+    }
+
+    public class Fia
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
